4d6879c2f8e63f4dc88f23a78fa23f180de4b05e
Thaina Mai <mnagahama21@gmail.com>
2020-08-04 16:48:58 -0300

    aula 4 finalizado

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
-const Error = () => (<div>Error</div>)
+const Error = () => (<div>Error</div>);
 
 ReactDOM.render(
 
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={Home} exact/>
+      <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
-      <Route path="/cadastro/categoria" component={CadastroCategoria} exact/>
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
       <Route component={Error} />
     </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,93 +1,138 @@
-import React, { useState } from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
-function CadastroCategoria(){
-  
-  const valoresIniciais ={
+function CadastroCategoria() {
+  const valoresIniciais = {
     nome: '',
     descricao: '',
-    cor:'', 
-  }
+    cor: '',
+  };
   const [values, setValues] = useState(valoresIniciais);
-  const [categorias,setCategorias] = useState([]);
+  const [categorias, setCategorias] = useState([]);
 
   function setValue(chave, valor) {
     setValues({
       ...values,
       [chave]: valor,
-    })
-  };
+    });
+  }
 
-  function handleChange(infoDoEvento){
+  function handleChange(infoDoEvento) {
     setValue(
       infoDoEvento.target.getAttribute('name'),
-      infoDoEvento.target.value
+      infoDoEvento.target.value,
     );
   }
-  
+
+  useEffect(() => {
+    if (window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias';
+      fetch(URL)
+        .then(async (respostaDoServer) => {
+          if (respostaDoServer.ok) {
+            const resposta = await respostaDoServer.json();
+            setCategorias(resposta);
+            return;
+          }
+          throw new Error('Não foi possível pegar os dados');
+        });
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setCategorias([
+  //       ...categorias,
+  //       {
+  //         id: 1,
+  //         titulo: 'Front End',
+  //         cor: '#6BD1FF',
+  //         link_extra: {
+  //           text: 'Formação de Front End na Alura',
+  //           url: 'https://www.alura.com.br/cursos-online-front-end',
+  //         },
+  //       },
+  //       {
+  //         id: 2,
+  //         titulo: 'Back End',
+  //         cor: '#00C86F',
+  //         link_extra: {
+  //           text: 'Cursos de Back End na Alura',
+  //           url: 'https://www.alura.com.br/cursos-online-programacao',
+  //         },
+  //       },
+  //     ]);
+  //   }, 4 * 1000);
+  // }, []);
+
   return (
-      <PageDefault>
-        <h1>Cadastro de Categoria: {values.nome} </h1>
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
 
-        <form onSubmit={function handleSubmit(infoDoEvento){
-          infoDoEvento.preventDefault();
-          setCategorias([
-            ...categorias,
-            values
-          ]);
+      <form onSubmit={function handleSubmit(infoDoEvento) {
+        infoDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          values,
+        ]);
 
-          setValues({valoresIniciais})
-        }}>
-          <FormField 
+        setValues({ valoresIniciais });
+      }}
+      >
+        <FormField
           label="Nome da categoria"
-          type="text"
+          type="textarea"
           name="nome"
-          value = {values.nome}
+          value={values.nome}
           onChange={handleChange}
-          />
+        />
 
-          <div>
-            <label>
-              Descriçao:
-              <textarea
-                type="text"
-                value={values.descricao}
-                name="descricao"
-                onChange={handleChange}
-              />
-            </label>
-          </div>
+        <FormField
+          label="Descrição:"
+          type="????"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
 
-          <FormField 
+        <FormField
           label="Cor"
           type="color"
           name="cor"
-          value = {values.cor}
+          value={values.cor}
           onChange={handleChange}
-          />
+        />
 
-        <button>
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
 
-      <ul>
-        {categorias.map((categoria, indice)=>{
-          return (
-            <li key={`${categoria}${indice}`}>
-              {categoria.nome}
-            </li>
-          )
-        })}
-      </ul>
-      
-        <Link to="/">
-          Home
-        </Link>
-      </PageDefault>
-    );
-  };
+      {categorias.length === 0 && (
+      <div>
+        Loading...
+      </div>
+      )}
 
-  export default CadastroCategoria;
+      <ul>
+        {categorias.map((categoria, indice) => (
+          <li key={`${categoria}${indice}`}>
+            {categoria.titulo}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  );
+}
+
+export default CadastroCategoria;
